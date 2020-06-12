f40503e084629d50dd201a96b24b7975512c09e6
KOSEUNGBIN <kosb15@naver.com>
2020-06-12 22:27:29 +0900

    Updates

---
+++ precache-manifest.a60a1036e442ca484d928884a788eb3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586dd4a325834497a124d8b34ed1a64a",
+    "url": "/todo-frontend/index.html"
+  },
+  {
+    "revision": "7d105492525d710e1d74",
+    "url": "/todo-frontend/static/css/main.c4d2baf7.chunk.css"
+  },
+  {
+    "revision": "0a6c73604e8ed316cd09",
+    "url": "/todo-frontend/static/js/2.67b48fb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-frontend/static/js/2.67b48fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d105492525d710e1d74",
+    "url": "/todo-frontend/static/js/main.b396afa3.chunk.js"
+  },
+  {
+    "revision": "47ff1e5a46d6edab2ad2",
+    "url": "/todo-frontend/static/js/runtime-main.41c1647a.js"
+  }
+]);
